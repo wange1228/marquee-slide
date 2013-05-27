1b636ef95c5d98e593708864e7c2ef3c068dbac1
WanGe <i@wange.im>
2013-05-27 15:29:04 +0800

    '部署grunt'

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+    var pkg = grunt.file.readJSON('package.json'),
+        readMe = '# ' + pkg.name + ' - ' + pkg.description                                                  + '\n' +
+                 '<pre>'                                                                                    + '\n' +
+                 '/***'                                                                                     + '\n' +
+                 ' @params auto: true                     # 是否自动滚动'                                   + '\n' +
+                 '         interval: 3000                 # 间隔时间（毫秒）'                               + '\n' +
+                 '         direction: "forward"           # 向前 -  forward / 向后 - backward'              + '\n' +
+                 '         speed: 500                     # 移动速度（毫秒）'                               + '\n' +
+                 '         showNum: 1                     # 显示个数'                                       + '\n' +
+                 '         stepLen: 1                     # 每次滚动步长'                                   + '\n' +
+                 '         type: "horizontal"             # 水平滚动 - horizontal / 垂直滚动 - vertical'    + '\n' +
+                 '         prevElement: null              # 上一组按钮元素'                                 + '\n' +
+                 '         prevBefore: function() {}      # 上一组移动前回调'                               + '\n' +
+                 '         prevAfter: function() {}       # 上一组移动后回调'                               + '\n' +
+                 '         nextElement: null              # 下一组按钮元素'                                 + '\n' +
+                 '         nextBefore: function() {}      # 下一组移动前回调'                               + '\n' +
+                 '         nextAfter: function() {}       # 下一组移动后回调'                               + '\n' +
+                 '         pauseElement: null             # 暂停按钮元素'                                   + '\n' +
+                 '         pauseBefore: function() {}     # 暂停前回调'                                     + '\n' +
+                 '         pauseAfter: function() {}      # 暂停后回调'                                     + '\n' +
+                 '         resumeElement: null            # 继续按钮元素'                                   + '\n' +
+                 '         resumeBefore: function() {}    # 继续前回调'                                     + '\n' +
+                 '         resumeAfter: function() {}     # 继续后回调'                                     + '\n' +
+                 ' @url ' + pkg.homepage                                                                    + '\n' +
+                 ' @version ' + pkg.version                                                                 + '\n' +
+                 ' @author ' + pkg.author.name                                                              + '\n' +
+                 ' @blog ' + pkg.author.url                                                                 + '\n' +
+                 '***/</pre>'                                                                               + '\n' +
+                 '## DEMO 预览'                                                                             + '\n' +
+                 pkg.homepage                                                                               + '\n';
+    grunt.initConfig({
+        pkg: pkg,
+        coffee: {
+            compileBare: {
+                options: {
+                    bare: true
+                },
+                files: {
+                    'src/marquee.source.js': 'src/marquee.source.coffee'
+                }
+            }
+        },
+        
+        uglify: {
+            options: {
+                banner: '/***'                                     + '\n' +
+                        '   @name <%= pkg.name %>'                 + '\n' +
+                        '   @description <%= pkg.description %>'   + '\n' +
+                        '   @url <%= pkg.homepage %>'              + '\n' +
+                        '   @version <%= pkg.version %>'           + '\n' +
+                        '   @author <%= pkg.author.name %>'        + '\n' +
+                        '   @blog <%= pkg.author.url %>'           + '\n' +
+                        '***/'                                     + '\n'
+            },
+            my_target: {
+                files: {
+                    'dest/marquee.js': 'src/marquee.source.js'
+                }
+            }
+        },
+        
+        watch: {
+            script: {
+                files: ['src/*.js'],
+                tasks: ['uglify']
+            },
+            coffee: {
+                files: ['src/*.coffee'],
+                tasks: ['coffee']
+            }
+        }
+    });
+    
+    
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['coffee', 'uglify', 'watch']);
+    grunt.file.write('README.md', readMe);
+};
--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# marquee.js - jQuery 多功能无缝滚动插件
+# Marquee-Slide - 基于 jQuery 的多功能无缝滚动插件
 <pre>
 /***
- @description 基于 jQuery 的多功能无缝滚动插件
  @params auto: true                     # 是否自动滚动
          interval: 3000                 # 间隔时间（毫秒）
-         direction: 'forward'           # 向前 -  forward / 向后 - backward
+         direction: "forward"           # 向前 -  forward / 向后 - backward
          speed: 500                     # 移动速度（毫秒）
          showNum: 1                     # 显示个数
          stepLen: 1                     # 每次滚动步长
-         type: 'horizontal'             # 水平滚动 - horizontal / 垂直滚动 - vertical
+         type: "horizontal"             # 水平滚动 - horizontal / 垂直滚动 - vertical
          prevElement: null              # 上一组按钮元素
          prevBefore: function() {}      # 上一组移动前回调
          prevAfter: function() {}       # 上一组移动后回调
@@ -21,11 +20,10 @@
          resumeElement: null            # 继续按钮元素
          resumeBefore: function() {}    # 继续前回调
          resumeAfter: function() {}     # 继续后回调
- @author i@wange.im
- @url https://github.com/wange1228/marquee-slide
+ @url http://wange.im/demo/marquee-slide/marquee_slide.html
+ @version 0.4.3
+ @author 万戈
  @blog http://wange.im
- @version 0.4.2
 ***/</pre>
-
 ## DEMO 预览
 http://wange.im/demo/marquee-slide/marquee_slide.html
